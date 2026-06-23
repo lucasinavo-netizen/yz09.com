2c068c00046e9a087c4ae9cc7a3fe8e573ea1e59
SEO Specialist <lucasinavo@gmail.com>
2026-06-23 12:56:26 +0800

    SEO: add affiliate edge redirect routes

---
+++ netlify/edge-functions/go-y2.js
@@ -0,0 +1,6 @@
+// Affiliate redirect: MM38AA branch.
+export default async () => Response.redirect('https://www.mm38aa.com/m/home?affiliateCode=lu0001', 302);
+
+export const config = {
+  path: '/go/y2',
+};
