ea3a17b1f78a151292d85f41015df62f6acb8e22
SEO Specialist <lucasinavo@gmail.com>
2026-06-23 13:06:04 +0800

    Build: use webpack-compatible Netlify build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
